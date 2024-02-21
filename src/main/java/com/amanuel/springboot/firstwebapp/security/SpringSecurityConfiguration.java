fcc584d49a55a30a2b820c859a9c6afc688444c0
amtenu <amannov21@gmail.com>
2024-02-21 10:46:27 -0700

    changed depricated USER.defaultPassEndode with Bcrypt encoder

--- src/main/java/com/amanuel/springboot/firstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/amanuel/springboot/firstwebapp/security/SpringSecurityConfiguration.java
@@ -1,26 +1,34 @@
 package com.amanuel.springboot.firstwebapp.security;
 
+import java.util.function.Function;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SpringSecurityConfiguration {
-	
+
 //	InMemoryUserDetailsManager 
 //	InMemoryUserDetailsManager(UserDetails...users)
-	
+
 	@Bean
 	public InMemoryUserDetailsManager createUserDetailsManager() {
-	UserDetails userDetail=	User.withDefaultPasswordEncoder()
-		.username("Amanuel")
-	    .password("1234")
-	    .roles("USER","ADMIN")
-	    .build();
+		Function<String, String> passwordEncoder = input ->passwordEncoder().encode(input);
+		UserDetails userDetail = User.builder().passwordEncoder(passwordEncoder ).username("Amanuel").password("1234")
+				.roles("USER", "ADMIN").build();
+
+		return new InMemoryUserDetailsManager(userDetail);
+	}
 	
-	return new InMemoryUserDetailsManager(userDetail);
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new  BCryptPasswordEncoder(); 
 	}
 
 }
